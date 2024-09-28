fbd16326b6cef22725c75b19e0ede90bc932d318
Anna <iaanya27@gmail.com>
2024-09-28 18:49:52 +0300

    added generateContacts

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,19 @@
-const generateContacts = async (number) => {};
+import { readContacts } from '../utils/readContacts';
+import { createFakeContact } from '../utils/createFakeContact';
+import { writeContacts } from '../utils/writeContacts';
+
+const generateContacts = async (number) => {
+  try {
+    const myContacts = await readContacts();
+    const newContacts = [];
+    for (let i = 0; i < number; i++) {
+      newContacts.push(createFakeContact());
+    }
+    const updatedContacts = [...myContacts, ...newContacts];
+    await writeContacts(updatedContacts);
+  } catch {
+    (error) => console.error(error);
+  }
+};
 
 generateContacts(5);
--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,9 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+    const contacts = await fs.readFile(PATH_DB, 'utf-8');
+const data = JSON.parse(contacts);
+    console.log(data);
+    
+};
