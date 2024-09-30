46f154efc9ce03261bb33791733eda3fa3f90d61
Anna <iaanya27@gmail.com>
2024-09-30 09:39:55 +0300

    added scripts

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -2,5 +2,10 @@ import { PATH_DB } from '../constants/contacts.js';
 import * as fs from 'node:fs/promises';
 
 export const writeContacts = async (updatedContacts) => {
-    const newContacts = await fs.writeFile(PATH_DB, data);
+    try {
+      const data = JSON.stringify(updatedContacts, undefined, 2);
+      await fs.writeFile(PATH_DB, data, 'utf-8');
+    } catch (error) {
+      console.log(error);
+    }
 };
